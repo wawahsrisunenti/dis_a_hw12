9c305b745a3d4805eff2461af6f1179549c45a1e
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-08 15:09:28 +0700

    feat: integrate Chakra UI components into the game board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { gameSlice } from "./reduxToolkit"; // Import gameSlice dari reduxToolkit
+import { Button, Text, Grid, GridItem } from "@chakra-ui/react"; // Impor komponen Chakra UI yang diperlukan
 import "./App.css";
 
 function Board() {
@@ -10,36 +11,46 @@ function Board() {
 
   function renderSquare(i) {
     return (
-      <button
-        className="square"
+      <Button
+        bg="blue.500"
+        color="white"
+        fontWeight="bold"
+        p="2"
+        rounded="md"
         onClick={() => dispatch(gameSlice.actions.selectSquare(i))}
       >
         {squares[i]}
-      </button>
+      </Button>
     );
   }
 
   return (
-    <div>
-      <div>STATUS: {status}</div>
-      <div>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      <button onClick={() => dispatch(gameSlice.actions.reset())}>
+    <div className="text-center my-8">
+      <Text fontSize="2xl" fontWeight="bold" mb="4">
+        STATUS: {status}
+      </Text>
+      <Grid templateColumns="repeat(3, 1fr)" gap="4">
+        <GridItem>{renderSquare(0)}</GridItem>
+        <GridItem>{renderSquare(1)}</GridItem>
+        <GridItem>{renderSquare(2)}</GridItem>
+        <GridItem>{renderSquare(3)}</GridItem>
+        <GridItem>{renderSquare(4)}</GridItem>
+        <GridItem>{renderSquare(5)}</GridItem>
+        <GridItem>{renderSquare(6)}</GridItem>
+        <GridItem>{renderSquare(7)}</GridItem>
+        <GridItem>{renderSquare(8)}</GridItem>
+      </Grid>
+      <Button
+        bg="red.500"
+        color="white"
+        fontWeight="bold"
+        p="2"
+        mt="4"
+        rounded="md"
+        onClick={() => dispatch(gameSlice.actions.reset())}
+      >
         Restart
-      </button>
+      </Button>
     </div>
   );
 }
