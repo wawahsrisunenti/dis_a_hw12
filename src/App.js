b5eca5f429e25898d513065006dd74c422ef0d2d
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-08 16:51:22 +0700

    style: improve UI styling in the Tic Tac Toe game

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ function Board() {
   function renderSquare(i) {
     return (
       <Button
-        bg="blue.500"
+        _hover={{
+          bgColor: "blue.200",
+          color: "red",
+          borderRadius: 0,
+        }}
+        textShadow="1px 1px #ff0000"
+        bg="yellow.500"
         color="white"
         fontWeight="bold"
         p="2"
@@ -25,34 +31,40 @@ function Board() {
   }
 
   return (
-    <Center h="100vh">
-      <div>
-        <Text fontSize="2xl" fontWeight="bold" mb="4">
-          STATUS: {status}
+    <Center h="100vh" flexDirection="column">
+      <Center
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        mb="4"
+      >
+        <Text fontSize="2xl" fontWeight="bold">
+          {status}
         </Text>
-        <Grid templateColumns="repeat(3, 1fr)" gap="4">
-          <GridItem>{renderSquare(0)}</GridItem>
-          <GridItem>{renderSquare(1)}</GridItem>
-          <GridItem>{renderSquare(2)}</GridItem>
-          <GridItem>{renderSquare(3)}</GridItem>
-          <GridItem>{renderSquare(4)}</GridItem>
-          <GridItem>{renderSquare(5)}</GridItem>
-          <GridItem>{renderSquare(6)}</GridItem>
-          <GridItem>{renderSquare(7)}</GridItem>
-          <GridItem>{renderSquare(8)}</GridItem>
-        </Grid>
+      </Center>
+      <Grid templateColumns="repeat(3, 1fr)" gap="4">
+        <GridItem>{renderSquare(0)}</GridItem>
+        <GridItem>{renderSquare(1)}</GridItem>
+        <GridItem>{renderSquare(2)}</GridItem>
+        <GridItem>{renderSquare(3)}</GridItem>
+        <GridItem>{renderSquare(4)}</GridItem>
+        <GridItem>{renderSquare(5)}</GridItem>
+        <GridItem>{renderSquare(6)}</GridItem>
+        <GridItem>{renderSquare(7)}</GridItem>
+        <GridItem>{renderSquare(8)}</GridItem>
+      </Grid>
+      <Center justifyContent="center" alignItems="center" mt="4">
         <Button
           bg="red.500"
           color="white"
           fontWeight="bold"
           p="2"
-          mt="4"
           rounded="md"
           onClick={() => dispatch(gameSlice.actions.reset())}
         >
           Restart
         </Button>
-      </div>
+      </Center>
     </Center>
   );
 }
