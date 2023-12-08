a4da317bb2bbd3e5c984dd26cd87a72b97303d1b
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-08 14:37:59 +0700

    chore: update ReactDOM rendering with createRoot
    Replace ReactDOM.render with createRoot for rendering the React app, following the latest React guidelines. This change is made to improve the app's performance and leverage the new features provided by createRoot.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux"; // Import Provider untuk Redux
 import store from "./store"; // Import store Redux yang telah dibuat
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 reportWebVitals();
